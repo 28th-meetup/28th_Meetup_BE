57629e61e7b2868c7fd0e09d8f89a9fc734fb2d9
eckrin <bsh51515@naver.com>
2023-10-01 17:13:15 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/kusitms/jipbap/JipbapApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.jipbap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JipbapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JipbapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusitms/jipbap/JipbapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.jipbap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JipbapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
