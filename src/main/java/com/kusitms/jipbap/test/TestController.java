82dcdf425258b60c0f85c85c9a2dacafa9c8de25
eckrin <bsh51515@naver.com>
2023-10-01 18:25:27 +0900

    chore: ci/cd 초기설정

--- src/main/java/com/kusitms/jipbap/test/TestController.java
+++ src/main/java/com/kusitms/jipbap/test/TestController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    /**
+     * 커넥션 테스트용 컨트롤러
+     * @return 성공시 String 반환
+     */
     @GetMapping("/test")
     public String test() {
         return "Connection Success";
