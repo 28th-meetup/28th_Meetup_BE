c4444825a4f8852de30b38bf7e87e9b995c85550
summit45 <brandy2145@gmail.com>
2023-11-16 22:05:56 +0900

    feat: 닉네임 중복여부 확인 api 기능 구현

--- src/main/java/com/kusitms/jipbap/user/UserController.java
+++ src/main/java/com/kusitms/jipbap/user/UserController.java
@@ -3,9 +3,11 @@ package com.kusitms.jipbap.user;
 import com.kusitms.jipbap.common.response.CommonResponse;
 import com.kusitms.jipbap.security.Auth;
 import com.kusitms.jipbap.security.AuthInfo;
+import com.kusitms.jipbap.user.dto.NicknameValidateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,12 @@ public class UserController {
     public CommonResponse<String> logout(@Auth AuthInfo authInfo) {
         userService.logout(authInfo.getEmail());
         return new CommonResponse<>("로그아웃 성공");
+    }
+
+    @Operation(summary = "닉네임 중복 여부 확인")
+    @PostMapping("/nickname")
+    public CommonResponse<String> checkNicknameIsDuplicate(@RequestBody NicknameValidateRequest dto) {
+        return new CommonResponse<>(userService.checkNicknameIsDuplicate(dto.getNickname()));
     }
 }
 
---
+++ src/main/java/com/kusitms/jipbap/user/dto/NicknameValidateRequest.java
@@ -0,0 +1,14 @@
+package com.kusitms.jipbap.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NicknameValidateRequest {
+    private String nickname;
+}
