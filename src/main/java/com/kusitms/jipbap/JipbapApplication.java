3f74a320c8e34f90d8f791dba7f005aa078b2a01
eckrin <bsh51515@naver.com>
2023-12-08 15:52:46 +0900

    feat: 비동기 @EnableAsync 세팅

--- src/main/java/com/kusitms/jipbap/JipbapApplication.java
+++ src/main/java/com/kusitms/jipbap/JipbapApplication.java
@@ -3,6 +3,7 @@ package com.kusitms.jipbap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //@EnableWebSocketMessageBroker
 //@EnableRedisRepositories
 @SpringBootApplication
+@EnableAsync
 public class JipbapApplication {
 
 	public static void main(String[] args) {
